22d7cfe4cb2f0e4e845fc7a42631575b92f7df45
sniperHunter <64271346+sniperHunter@users.noreply.github.com>
2020-06-21 11:59:24 +0100

    Update JSONtoCSVgui.py

--- JSONtoCSVgui.py
+++ JSONtoCSVgui.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 root= tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue2', relief = 'raised')
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'khaki2', relief = 'raised')
 canvas1.pack()
 
-label1 = tk.Label(root, text='JSON to CSV Conversion Tool', bg = 'lightsteelblue2')
-label1.config(font=('helvetica', 20))
+label1 = tk.Label(root, text='JSON to CSV Conversion Tool', bg = 'khaki2')
+label1.config(font=('helvetica', 14))
 canvas1.create_window(150, 60, window=label1)
 
 def getJSON ():
@@ -18,7 +18,7 @@ def getJSON ():
     import_file_path = filedialog.askopenfilename()
     read_file = pd.read_json (import_file_path)
     
-browseButton_JSON = tk.Button(text="      Import JSON File     ", command=getJSON, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+browseButton_JSON = tk.Button(text="      Import JSON File     ", command=getJSON, bg='salmon', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 130, window=browseButton_JSON)
 
 def convertToCSV ():
@@ -27,7 +27,7 @@ def convertToCSV ():
     export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
     read_file.to_csv (export_file_path, index = None, header=True)
 
-saveAsButton_CSV = tk.Button(text='Convert JSON to CSV', command=convertToCSV, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+saveAsButton_CSV = tk.Button(text='Convert JSON to CSV', command=convertToCSV, bg='salmon', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 180, window=saveAsButton_CSV)
 
 def exitApplication():
@@ -35,7 +35,7 @@ def exitApplication():
     if MsgBox == 'yes':
        root.destroy()
      
-exitButton = tk.Button (root, text='       Exit Application     ',command=exitApplication, bg='brown', fg='white', font=('helvetica', 12, 'bold'))
+exitButton = tk.Button (root, text='       Exit Application     ',command=exitApplication, bg='red2', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 230, window=exitButton)
 
 root.mainloop()
